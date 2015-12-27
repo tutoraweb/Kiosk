f4417cb87f5eeb63f8949eb669b5694db248468e
skrzepto <shims506@gmail.com>
2015-12-27 16:45:31 +0000

    (#11) Text box in home page for signin/signout
    Reviewed by: Serapheim Dimitropoulos <serapheimd@gmail.com>

--- kiosk.py
+++ kiosk.py
@@ -13,15 +13,28 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright (c) 2015, Serapheim Dimitropoulos
+# Copyright (c) 2015, Szymon Krzeptowski-Mucha
 #
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return render_template('hello.html')
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    error = None
+    success = None
+    if request.method == 'POST':
+        if request.form['anumber'] != '12345678':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            success = 'Signed In successfully'
+    return render_template('signin.html', error=error, success=success)
 
 if __name__ == "__main__":
     app.run()
